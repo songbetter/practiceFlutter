29a80d09e13f79a857f19a7121184d5ff3e0d29f
songannie <annie@alethio.io>
2022-04-08 00:38:08 +0900

    practice flutter

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:annieflutter/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomeScreen(),
+  ));
+}
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class HomeScreen extends StatelessWidget {
+  WebViewController? controller;
+
+  final homeUrl = "https://dev-www.babyface.io";
+
+  HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            backgroundColor: Colors.orange,
+            title: Text("annieWebview"),
+            centerTitle: true,
+            actions: [
+              IconButton(
+                  onPressed: () {
+                    if (controller == null) {
+                      return;
+                    }
+                    controller?.loadUrl(homeUrl);
+                  },
+                  icon: Icon(Icons.home))
+            ]),
+        body: WebView(
+          onWebViewCreated: (WebViewController controller) {
+            this.controller = controller;
+          },
+          initialUrl: homeUrl,
+          javascriptMode: JavascriptMode.unrestricted,
+        ));
+  }
+}
