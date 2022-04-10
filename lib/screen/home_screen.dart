d648e35cdc396f696929883779265b882760884e
songannie <annie@alethio.io>
2022-04-10 21:05:46 +0900

    practice: statefulWidget

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,36 +1,71 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
-class HomeScreen extends StatelessWidget {
-  WebViewController? controller;
+class HomeScreen extends StatefulWidget {
+  final Color color;
 
-  final homeUrl = "https://dev-www.babyface.io";
+  HomeScreen({
+    required this.color,
+    Key? key,
+  }) : super(key: key) {
+    print("Contstructor 실행");
+  }
 
-  HomeScreen({Key? key}) : super(key: key);
+  @override
+  _HomeScreenState createState() {
+    print("createState 실행");
+    return _HomeScreenState();
+  }
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int number = 0;
+
+  @override
+  void initState() {
+    print("initState 실행");
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    print("didChangeDependencies 실행");
+    super.didChangeDependencies();
+  }
+
+  @override
+  void didUpdateWidget(covariant HomeScreen oldWidget) {
+    print("didUpdateWidget 실행");
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void deactivate() {
+    print("deactivate 실행");
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    print("dispose실행");
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-            backgroundColor: Colors.orange,
-            title: Text("annieWebview"),
-            centerTitle: true,
-            actions: [
-              IconButton(
-                  onPressed: () {
-                    if (controller == null) {
-                      return;
-                    }
-                    controller?.loadUrl(homeUrl);
-                  },
-                  icon: Icon(Icons.home))
-            ]),
-        body: WebView(
-          onWebViewCreated: (WebViewController controller) {
-            this.controller = controller;
-          },
-          initialUrl: homeUrl,
-          javascriptMode: JavascriptMode.unrestricted,
+    print("build 실행");
+    return GestureDetector(
+        onTap: () {
+          setState(() {
+            number++;
+          });
+        },
+        child: Container(
+          width: 50.0,
+          height: 50.0,
+          color: widget.color,
+          child: Center(
+            child: Text(number.toString()),
+          ),
         ));
   }
 }
