a96cabb4255abc518c2dea439888ba18a05b6cd6
songbetter <5ongnaeu17@gmail.com>
2022-04-12 01:22:10 +0900

    apply: 프로젝트 한 곳에 모으기 - HomeScreen 변경 - List<Map<String, dynamic>> value 값 가져오기

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,130 +1,113 @@
-import 'dart:math';
-
-import 'package:annieflutter/constant/color.dart';
-import 'package:annieflutter/screen/settings_screen.dart';
+import 'package:annieflutter/screen/datePicker/datePicker_main_screen.dart';
+import 'package:annieflutter/screen/photoAlbum/photoAlbum_main_screen.dart';
+import 'package:annieflutter/screen/random/random_main_screen.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  int maxNumber = 1000;
-  List<int> randomNumbers = [123, 456, 789];
-
-  @override
   Widget build(BuildContext context) {
+    List<Map<String, dynamic>> icons = [
+      {
+        "color": Colors.lightGreen,
+        "route": PhotoAlbumMainScreen(),
+        "icon": Icons.photo_album,
+        "title": "액자"
+      },
+      {
+        "color": Colors.pinkAccent,
+        "route": DatePickerMainScreen(),
+        "icon": Icons.date_range,
+        "title": "U & I"
+      },
+      {
+        "color": Colors.orange,
+        "route": RandomMainScreen(),
+        "icon": Icons.workspaces_filled,
+        "title": "랜덤숫자"
+      },
+    ];
     return Scaffold(
-      backgroundColor: PRIMARY_COLOR,
-      body: Container(
-          width: MediaQuery.of(context).size.width,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                _Header(onPressed: onSettingsPop),
-                _Body(randomNumbers: randomNumbers),
-                _Footer(onPressed: onPressedCreateButton),
-              ],
-            ),
-          )),
-    );
-  }
-
-  void onPressedCreateButton() {
-    final rand = Random();
-
-    final Set<int> newNumbers = {}; // 중복 리스트를 없애기 위해 List 대신 Set 사용
-
-    while (newNumbers.length != 3) {
-      // 3개의 숫자가 모두 나올 수 있도록, for 대신 while 루프 사용
-      final number = rand.nextInt(maxNumber); // 최대값: 1000
-
-      newNumbers.add(number);
-    }
-
-    setState(() {
-      randomNumbers = newNumbers.toList();
-    });
-  }
-
-  void onSettingsPop() async {
-    final result = await Navigator.of(context)
-        .push(MaterialPageRoute(builder: (BuildContext context) {
-      return SettingsScreen(maxNumber: maxNumber);
-    }));
-
-    if (result != null) {
-      setState(() {
-        maxNumber = result;
-      });
-    }
-  }
-}
-
-class _Header extends StatelessWidget {
-  final VoidCallback onPressed;
-
-  const _Header({required this.onPressed, Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text(
-          "랜덤숫자 생성기",
-          style: TextStyle(color: Colors.white, fontSize: 20.0),
-        ),
-        IconButton(
-          onPressed: onPressed,
-          icon: Icon(
-            Icons.settings,
-            color: Colors.red,
-          ),
-        )
-      ],
-    );
-  }
-}
-
-class _Body extends StatelessWidget {
-  final List<int> randomNumbers;
-
-  const _Body({required this.randomNumbers, Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: randomNumbers
-              .map((x) =>
-                  Text(x.toString(), style: TextStyle(color: Colors.white)))
-              .toList()),
-    );
-  }
-}
-
-class _Footer extends StatelessWidget {
-  final VoidCallback onPressed;
-
-  const _Footer({required this.onPressed, Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-        width: double.infinity,
-        child: ElevatedButton(
-          style: ElevatedButton.styleFrom(primary: Colors.red),
-          onPressed: onPressed,
-          child: Text(
-            "생성하기",
-          ),
-        ));
+        body: Container(
+            width: MediaQuery.of(context).size.width,
+            child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                          padding: const EdgeInsets.only(bottom: 32.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: icons
+                                .asMap()
+                                .entries
+                                .map((icon) => Column(
+                                      children: [
+                                        Container(
+                                          decoration: BoxDecoration(
+                                              border: Border.all(
+                                                  color: icon.value["color"]),
+                                              shape: BoxShape.circle),
+                                          child: IconButton(
+                                            onPressed: () {
+                                              Navigator.of(context).push(
+                                                  MaterialPageRoute(builder:
+                                                      (BuildContext context) {
+                                                return icon.value["route"];
+                                              }));
+                                            },
+                                            icon: Icon(icon.value["icon"]),
+                                            color: icon.value["color"],
+                                            iconSize: 50.0,
+                                          ),
+                                        ),
+                                        Text(
+                                          icon.value["title"],
+                                          style: TextStyle(
+                                              color: icon.value["color"],
+                                              fontSize: 20.0),
+                                        ),
+                                      ],
+                                    ))
+                                .toList(),
+                          )),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: icons
+                            .asMap()
+                            .entries
+                            .map((icon) => Column(
+                                  children: [
+                                    Container(
+                                      decoration: BoxDecoration(
+                                          border: Border.all(
+                                              color: icon.value["color"]),
+                                          shape: BoxShape.circle),
+                                      child: IconButton(
+                                        onPressed: () {
+                                          Navigator.of(context).push(
+                                              MaterialPageRoute(builder:
+                                                  (BuildContext context) {
+                                            return icon.value["route"];
+                                          }));
+                                        },
+                                        icon: Icon(icon.value["icon"]),
+                                        color: icon.value["color"],
+                                        iconSize: 50.0,
+                                      ),
+                                    ),
+                                    Text(
+                                      icon.value["title"],
+                                      style: TextStyle(
+                                          color: icon.value["color"],
+                                          fontSize: 20.0),
+                                    ),
+                                  ],
+                                ))
+                            .toList(),
+                      )
+                    ]))));
   }
 }
---
+++ lib/screen/random/random_main_screen.dart
@@ -0,0 +1,137 @@
+import 'dart:math';
+
+import 'package:annieflutter/constant/color.dart';
+import 'package:annieflutter/screen/random/settings_screen.dart';
+import 'package:flutter/material.dart';
+
+class RandomMainScreen extends StatefulWidget {
+  const RandomMainScreen({Key? key}) : super(key: key);
+
+  @override
+  State<RandomMainScreen> createState() => _RandomMainScreenState();
+}
+
+class _RandomMainScreenState extends State<RandomMainScreen> {
+  int maxNumber = 1000;
+  List<int> randomNumbers = [123, 456, 789];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: PRIMARY_COLOR,
+      body: Container(
+          width: MediaQuery.of(context).size.width,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _Header(onPressed: onSettingsPop),
+                _Body(randomNumbers: randomNumbers),
+                _Footer(onPressed: onPressedCreateButton),
+              ],
+            ),
+          )),
+    );
+  }
+
+  void onPressedCreateButton() {
+    final rand = Random();
+
+    final Set<int> newNumbers = {}; // 중복 리스트를 없애기 위해 List 대신 Set 사용
+
+    while (newNumbers.length != 3) {
+      // 3개의 숫자가 모두 나올 수 있도록, for 대신 while 루프 사용
+      final number = rand.nextInt(maxNumber); // 최대값: 1000
+
+      newNumbers.add(number);
+    }
+
+    setState(() {
+      randomNumbers = newNumbers.toList();
+    });
+  }
+
+  void onSettingsPop() async {
+    final result = await Navigator.of(context)
+        .push(MaterialPageRoute(builder: (BuildContext context) {
+      return SettingsScreen(maxNumber: maxNumber);
+    }));
+
+    if (result != null) {
+      setState(() {
+        maxNumber = result;
+      });
+    }
+  }
+}
+
+class _Header extends StatelessWidget {
+  final VoidCallback onPressed;
+
+  const _Header({required this.onPressed, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            icon: Icon(Icons.arrow_back_ios),
+            color: Colors.white,
+            iconSize: 20.0),
+        Text(
+          "랜덤숫자 생성기",
+          style: TextStyle(color: Colors.white, fontSize: 20.0),
+        ),
+        IconButton(
+          onPressed: onPressed,
+          icon: Icon(
+            Icons.settings,
+            color: Colors.red,
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  final List<int> randomNumbers;
+
+  const _Body({required this.randomNumbers, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: randomNumbers
+              .map((x) =>
+                  Text(x.toString(), style: TextStyle(color: Colors.white)))
+              .toList()),
+    );
+  }
+}
+
+class _Footer extends StatelessWidget {
+  final VoidCallback onPressed;
+
+  const _Footer({required this.onPressed, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        width: double.infinity,
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(primary: Colors.red),
+          onPressed: onPressed,
+          child: Text(
+            "생성하기",
+          ),
+        ));
+  }
+}
